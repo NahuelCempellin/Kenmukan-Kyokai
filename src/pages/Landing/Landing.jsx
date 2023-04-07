76ac6cf66b3df74fe4f1b8651f4073b5ca90e01f
Nahuel Cempellin <nahuelcempellin@gmail.com>
2023-04-07 12:31:57 -0300

    animacion

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,42 +1,44 @@
-import React from 'react'
-import '../../App.css'
-import Pensamiento from '../../components/QueEs/pensamiento/Pensamiento'
-import QueEs from '../../components/QueEs/QueEs'
-import Sedes from '../../components/sedes/Sedes'
-
+import React from "react";
+import "../../App.css";
+import Pensamiento from "../../components/QueEs/pensamiento/Pensamiento";
+import QueEs from "../../components/QueEs/QueEs";
+import Sedes from "../../components/sedes/Sedes";
+import "animate.css";
 
 const Landing = () => {
   return (
-    <div className='landing-container' id='landing'>
-    <div className='title-container'>
-            <div className='title'>
+    <div className="landing-container" id="landing">
+      <div id="title-container">
+        <div className="animate__animated animate__fadeInUp">
+          <div className="title">
             <h2>Bienvenidos a</h2>
             <h1>Kenmukan Kyokai</h1>
             <p>
-            Acérquese a conocer el mundo de la esgrima japonesa, e informarse acerca de nuestra organización y sus distintas sedes.
+              Acérquese a conocer el mundo de la esgrima japonesa, e informarse
+              acerca de nuestra organización y sus distintas sedes.
             </p>
 
-            <div className='button-container'>
-              <a href='#kendo'>¿Que es Kendo y Iaido?</a>
-              <a href='#sedes'>Sedes / horarios</a>
-            </div>
+            <div className="button-container">
+              <a href="#kendo">¿Que es Kendo y Iaido?</a>
+              <a href="#sedes">Sedes / horarios</a>
             </div>
           </div>
+        </div>
+      </div>
 
-            <div className='container-pensamiento' id='kendo'>
-                <QueEs/>
-            </div>
+      <div className="container-pensamiento" id="kendo">
+        <QueEs />
+      </div>
 
-            <div className='container-pensamiento' >
-                    <Pensamiento/>
-            </div>
+      <div className="container-pensamiento">
+        <Pensamiento />
+      </div>
 
-
-            <div className='container-pensamiento'> 
-                <Sedes/>
-            </div>
+      <div className="container-pensamiento">
+        <Sedes />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
