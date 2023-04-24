2b79c28a1bcbece64bd5e47ebdb3ba489cdee8bc
Nahuel Cempellin <nahuelcempellin@gmail.com>
2023-04-24 14:34:31 -0300

    kensei add0

--- src/components/sedes/mocked-data.js
+++ src/components/sedes/mocked-data.js
@@ -1,81 +1,91 @@
-import kmk from '../../assets/Logo.png'
-import joon from '../../assets/joon.png'
-import mdp from '../../assets/mdpBudokai.png'
+import kmk from "../../assets/Logo.png";
+import joon from "../../assets/joon.png";
+import mdp from "../../assets/mdpBudokai.png";
+import kensei from "../../assets/Kensei.png";
 
-
-
-
-export const CardData=[
-    {
-       dojo: 'Kenmu-Jingu',
-       jp:' 劍無-神宮',
-       image: kmk,
-       localizacion: 'Vte Lopez GBA',
-       ubicacion:'https://maps.app.goo.gl/yKnGSNzHtcgu2SSm9',
-       profesor: 'Gustavo Jacinto 5° Dan',
-       dias: 'Sabados, (Solo profesores e invitados especiales)',
-       direccion: 'Av Maipu 1100',
-       Horario:{
-           iaido: '14hs-16hs',
-           kendo:'16hs-17:30hs'
-       },
-       mail: 'kenmukan.kyokai@gmail.com',
-       facebook:'https://www.facebook.com/kenmukan',
-       instagram:'https://instagram.com/gustavo.jacinto.79?igshid=YmMyMTA2M2Y=',
+export const CardData = [
+  {
+    dojo: "Kenmu-Jingu",
+    jp: " 劍無-神宮",
+    image: kmk,
+    localizacion: "Vte Lopez GBA",
+    ubicacion: "https://maps.app.goo.gl/yKnGSNzHtcgu2SSm9",
+    profesor: "Gustavo Jacinto 5° Dan",
+    dias: "Sabados, (Solo profesores e invitados especiales)",
+    direccion: "Av Maipu 1100",
+    Horario: {
+      iaido: "14hs-16hs",
+      kendo: "16hs-17:30hs",
     },
-    {
-           dojo: 'Takashima',
-           jp:'高島',
-           image: kmk,
-           localizacion: 'CABA',
-           ubicacion:'https://maps.app.goo.gl/zen36wKRQXcrULWq7',
+    mail: "kenmukan.kyokai@gmail.com",
+    facebook: "https://www.facebook.com/kenmukan",
+    instagram: "https://instagram.com/gustavo.jacinto.79?igshid=YmMyMTA2M2Y=",
+  },
+  {
+    dojo: "Takashima",
+    jp: "高島",
+    image: kmk,
+    localizacion: "CABA",
+    ubicacion: "https://maps.app.goo.gl/zen36wKRQXcrULWq7",
 
-           profesor: 'Mariano Isla Casares 3° Dan',
-           dias: 'Jueves',
-           direccion: 'Gallo 246',
-           Horario:{
-               iaido: '21hs-23:30hs',
-               
-           },
-           mail: 'kenmukan.kyokai@gmail.com',
-           facebook:'https://www.facebook.com/kenmukan',
-           instagram:'https://instagram.com/gustavo.jacinto.79?igshid=YmMyMTA2M2Y=',
-        },
-        {
-           dojo: 'Joon Aang',
-           jp:'中央',
-           image: joon,
-           localizacion: 'Floresta',
-           ubicacion:'https://maps.app.goo.gl/8HgoVQXGQhDTeUEL6',
-           profesor: 'Lee Kyung Yul 5° Dan, Baik Soojin 5° Dan',
-           profesor2:'Baik Soojin 5° Dan',
-           categoria:'5 Dan',
-           dias: 'Lunes y Miercoles',
-           direccion: 'Bahia blanca 742 Esq.Gaona',
-           Horario:{
-               kendo: '21hs-23:30hs',
-               
-           },
-           mail: 'kenmukan.kyokai@gmail.com',
-           facebook:'https://www.facebook.com/kenmukan',
-           instagram:'https://www.instagram.com/joongaangdojo/',
-        },
-        {
-           dojo: 'MDP Budokai',
-           jp:'マル デル プラタ-武道会',
-           image: mdp,
-           localizacion: 'Mar del plata',
-           profesor: 'Juan Ignacio Fuster 3° Dan',
-           dias: 'Comunicarse',
-           direccion: 'Saavedra 3050',
-           Horario:{
-               kendo: '21hs-23:30hs',
-               },
-           mail: 'budokaimdp@gmail.com',
-           facebook:'https://www.facebook.com/kenmukan',
-           instagram:'https://www.instagram.com/joongaangdojo/',
-        },
-   
-   
-   
-       ]
+    profesor: "Mariano Isla Casares 3° Dan",
+    dias: "Jueves",
+    direccion: "Gallo 246",
+    Horario: {
+      iaido: "21hs-23:30hs",
+    },
+    mail: "kenmukan.kyokai@gmail.com",
+    facebook: "https://www.facebook.com/kenmukan",
+    instagram: "https://instagram.com/gustavo.jacinto.79?igshid=YmMyMTA2M2Y=",
+  },
+  {
+    dojo: "Joon Aang",
+    jp: "中央",
+    image: joon,
+    localizacion: "Floresta",
+    ubicacion: "https://maps.app.goo.gl/8HgoVQXGQhDTeUEL6",
+    profesor: "Lee Kyung Yul 5° Dan, Baik Soojin 5° Dan",
+    profesor2: "Baik Soojin 5° Dan",
+    categoria: "5 Dan",
+    dias: "Lunes y Miercoles",
+    direccion: "Bahia blanca 742 Esq.Gaona",
+    Horario: {
+      kendo: "21hs-23:30hs",
+    },
+    mail: "kenmukan.kyokai@gmail.com",
+    facebook: "https://www.facebook.com/kenmukan",
+    instagram: "https://www.instagram.com/joongaangdojo/",
+  },
+  {
+    dojo: "Kenseikan",
+    jp: "劍誠館",
+    image: kensei,
+    localizacion: "Mar del plata",
+    ubicacion:
+      "https://www.google.com/maps/dir//Av.+Constitución+5951,+B7600+Mar+del+Plata,+Provincia+de+Buenos+Aires/@-37.9604059,-57.6330325,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x9584d9e651d21615:0x2e2e9dec08ba3dab!2m2!1d-57.5629925!2d-37.9604267",
+    profesor: "Nahuel Felipe Cempellin 2° Dan",
+    dias: "Lunes, Miercoles y Viernes",
+    direccion: "Av.Constitucion 5951",
+    Horario: {
+      kendo: "16:00-17:30hs",
+    },
+    mail: "nahuelcempellin@gmail.com",
+    facebook: "",
+    instagram: "https://www.instagram.com/kensei.budo/?igshid=ZDdkNTZiNTM%3D",
+  },
+  {
+    dojo: "MDP Budokai",
+    jp: "マル デル プラタ-武道会",
+    image: mdp,
+    localizacion: "Mar del plata",
+    profesor: "Juan Ignacio Fuster 3° Dan",
+    dias: "Comunicarse",
+    direccion: "Saavedra 3050",
+    Horario: {
+      kendo: "21hs-23:30hs",
+    },
+    mail: "budokaimdp@gmail.com",
+    facebook: "https://www.facebook.com/kenmukan",
+    instagram: "https://www.instagram.com/joongaangdojo/",
+  },
+];
