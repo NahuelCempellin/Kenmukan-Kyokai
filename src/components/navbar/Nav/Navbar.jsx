5180b1157c6b3869e52b86333443e2142bd42f35
Nahuel Cempellin <nahuelcempellin@gmail.com>
2023-06-28 13:10:10 -0300

    cambio en imagenes y margenes

--- src/components/navbar/Nav/Navbar.jsx
+++ src/components/navbar/Nav/Navbar.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import Logo from '../../logo/Logo'
-import '../navbar.css'
-import {RxHamburgerMenu} from 'react-icons/rx'
-
+import React from "react";
+import Logo from "../../logo/Logo";
+import "../navbar.css";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   return (
-    <div className='nav-container'>
-        <Logo/>
+    <div className="nav-container">
+      <Logo />
 
+      <div className="item-container">
+        <a href="#landing">Home</a>
+        {/* <a>Noticias</a> */}
+        <a href="#kendo">Kendo / Iaido</a>
+        <a href="#sedes">Sedes</a>
+        <a href="#footer">Contacto</a>
+      </div>
 
-        <div className='item-container'>
-            <a href='#landing'>Home</a>
-            {/* <a>Noticias</a> */}
-            <a href='#kendo'>Kendo / Iaido</a>
-            <a href='#sedes'>Sedes</a>
-            <a href='#footer'>Contacto</a>
-        </div>
-
-
-        <div className='responsive-nav'>
-          {/* <RxHamburgerMenu/> */}
-        </div>
+      <div className="responsive-nav">
+        <RxHamburgerMenu />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
